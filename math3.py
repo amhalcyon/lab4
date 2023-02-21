b15f8e5a632a91017c9371a53778d8f27f2da832
amhalcyon <adinamedelbai@gmail.com>
2023-02-21 20:29:36 +0600

    second commit

--- math3.py
+++ math3.py
@@ -3,6 +3,6 @@ from math import tan, pi
 n = int(input("Number of sides: "))
 l = int(input("The length of a side: "))
 
-a = int(n*(l**2)/(4*tan(pi/n)))
+a = round(n*(l**2)/(4*tan(pi/n)))
 
 print("The area of the polygon is:", a)
